521088b19ce846ae5d7308827fbb101efaf9fdfa
Paul Blankson <paul.blankson@amalitech.com>
2024-12-04 23:45:53 +0000

    feat: added sidebar component to your info component

--- src/app/components/your-info/your-info.component.ts
+++ src/app/components/your-info/your-info.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-your-info',
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './your-info.component.html',
   styleUrl: './your-info.component.css'
 })
