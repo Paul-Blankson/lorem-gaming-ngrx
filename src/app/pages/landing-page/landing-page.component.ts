4e3854032d24a8e5d909b4e42e61c936a46dafe0
Paul Blankson <paul.blankson@amalitech.com>
2024-12-04 16:34:28 +0000

    feat: added sample landing page starter

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
 export class LandingPageComponent {
-
+  gamingFeatures = [
+    'Unlimited Game Access',
+    'Cloud Gaming',
+    'Exclusive Community',
+    '4K Streaming',
+    'Multi-Device Support'
+  ];
 }
