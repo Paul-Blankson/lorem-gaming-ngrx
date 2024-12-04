9d85612dcc4ed89aa9460f9573814d6565db211b
Paul Blankson <paul.blankson@amalitech.com>
2024-12-04 16:37:04 +0000

    feat: configured routes for each sign-up step

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { YourInfoComponent } from './components/your-info/your-info.component';
+import { SelectPlanComponent } from './components/select-plan/select-plan.component';
+import { AddOnsComponent } from './components/add-ons/add-ons.component';
+import { SummaryComponent } from './components/summary/summary.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  {
+    path: 'sign-up',
+    children: [
+      { path: '', redirectTo: 'your-info', pathMatch: 'full' },
+      { path: 'your-info', component: YourInfoComponent },
+      { path: 'select-plan', component: SelectPlanComponent },
+      { path: 'add-ons', component: AddOnsComponent },
+      { path: 'summary', component: SummaryComponent },
+    ],
+  },
+];
