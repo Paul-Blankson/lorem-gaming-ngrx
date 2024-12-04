f62893865512d180255aa635d86191aa28497153
Paul Blankson <paul.blankson@amalitech.com>
2024-12-04 16:35:40 +0000

    added component for each step

---
+++ src/app/components/your-info/your-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-your-info',
+  imports: [],
+  templateUrl: './your-info.component.html',
+  styleUrl: './your-info.component.css'
+})
+export class YourInfoComponent {
+
+}
