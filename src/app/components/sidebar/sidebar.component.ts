ec0dfb66bfeeb0ffcfddd22a48e966962fd29da9
Paul Blankson <paul.blankson@amalitech.com>
2024-12-08 20:21:20 +0000

    feat: added dynamic navigation to defined routes

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,9 +1,6 @@
-import { Component, Input } from '@angular/core';
-
-interface Step {
-  number: number;
-  label: string;
-}
+import { Component, inject, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Step } from '../../models';
 
 @Component({
   selector: 'app-sidebar',
@@ -13,11 +10,20 @@ interface Step {
 })
 export class SidebarComponent {
   @Input() currentStep: number = 1;
+  router: Router = inject(Router);
 
   steps: Step[] = [
-    { number: 1, label: 'YOUR INFO' },
-    { number: 2, label: 'SELECT PLAN' },
-    { number: 3, label: 'ADD-ONS' },
-    { number: 4, label: 'SUMMARY' }
+    { number: 1, label: 'YOUR INFO', route: '/sign-up/your-info' },
+    { number: 2, label: 'SELECT PLAN', route: '/sign-up/select-plan' },
+    { number: 3, label: 'ADD-ONS', route: '/sign-up/add-ons' },
+    { number: 4, label: 'SUMMARY', route: '/sign-up/summary' },
   ];
+
+  navigateToStep(step: Step): void {
+    this.router.navigate([step.route]);
+  }
+
+  isActive(path: string): boolean {
+    return this.router.url === path;
+  }
 }
