9163fdf1ec5660860e16bc4f63d49f6d8350cfa1
Paul Blankson <paul.blankson@amalitech.com>
2024-12-09 12:54:08 +0000

    feat: added a service to hold the state of the form data across each step

---
+++ src/app/service/form-data.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormDataService {
+  private formData: any = {
+    yourInfo: {
+      name: '',
+      email: '',
+      phone: '',
+    },
+    selectPlan: {
+      selectedPlan: 'Arcade',
+      isYearly: false,
+    },
+  };
+
+  getFormData(): any {
+    return this.formData;
+  }
+
+  setFormData(step: string, data: any): void {
+    this.formData[step] = data;
+  }
+}
