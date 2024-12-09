de7928f2390169b0af30eec3b990f349156df1e7
Paul Blankson <paul.blankson@amalitech.com>
2024-12-09 16:00:56 +0000

    feat: used method from formService to save data entered by user

--- src/app/components/your-info/your-info.component.ts
+++ src/app/components/your-info/your-info.component.ts
@@ -1,32 +1,61 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { FormDataService } from '../../service/form-data.service';
 @Component({
   selector: 'app-your-info',
-  imports: [SidebarComponent, ReactiveFormsModule, CommonModule],
+  imports: [SidebarComponent, ReactiveFormsModule, RouterModule, CommonModule],
   templateUrl: './your-info.component.html',
-  styleUrl: './your-info.component.css'
+  styleUrl: './your-info.component.css',
 })
 export class YourInfoComponent {
   yourInfoForm: FormGroup;
   fields = [
-    { label: 'Name', type: 'text', placeholder: 'e.g. Stephen King', name: 'name', validators: [Validators.required] },
-    { label: 'Email Address', type: 'email', placeholder: 'e.g. stephenking@lorem.com', name: 'email', validators: [Validators.required, Validators.email] },
-    { label: 'Phone Number', type: 'tel', placeholder: 'e.g. +1 234 567 890', name: 'phone', validators: [Validators.required] },
+    {
+      label: 'Name',
+      type: 'text',
+      placeholder: 'e.g. Stephen King',
+      name: 'name',
+      validators: [Validators.required],
+    },
+    {
+      label: 'Email Address',
+      type: 'email',
+      placeholder: 'e.g. stephenking@lorem.com',
+      name: 'email',
+      validators: [Validators.required, Validators.email],
+    },
+    {
+      label: 'Phone Number',
+      type: 'tel',
+      placeholder: 'e.g. +1 234 567 890',
+      name: 'phone',
+      validators: [Validators.required],
+    },
   ];
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private formDataService: FormDataService
+  ) {
     const formControls: { [key: string]: FormControl } = {};
-    this.fields.forEach(field => {
+    this.fields.forEach((field) => {
       formControls[field.name] = new FormControl('', field.validators);
     });
 
     this.yourInfoForm = this.fb.group(formControls);
   }
 
-  nextStep(){
+  nextStep() {
     this.router.navigate(['/sign-up/select-plan']);
   }
 
@@ -36,7 +65,8 @@ export class YourInfoComponent {
         this.yourInfoForm.controls[key].markAsTouched();
       });
     } else {
-      console.log('Form submitted:', this.yourInfoForm.value);
+      this.formDataService.setFormData('yourInfo', this.yourInfoForm.value);
+      this.nextStep();
     }
   }
 }
