62e83c00a0b03ff7371bc1bf2fb8782ccb032bd7
Paul Blankson <paul.blankson@amalitech.com>
2024-12-11 02:25:35 +0000

    refactor: retrieve isYearly data from Form data service

--- src/app/components/add-ons/add-ons.component.ts
+++ src/app/components/add-ons/add-ons.component.ts
@@ -1,22 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { Router } from '@angular/router';
 import { FormDataService } from '../../service/form-data.service';
-
+import { CurrencyPipe } from '@angular/common';
 @Component({
   selector: 'app-add-ons',
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, CurrencyPipe],
   templateUrl: './add-ons.component.html',
   styleUrl: './add-ons.component.css'
 })
-export class AddOnsComponent {
+export class AddOnsComponent implements OnInit {
+  isYearly: boolean = false;
   addOns = [
-    { name: 'Online service', description: 'Access to multiplayer games', price: '$1/mo', isSelected: false },
-    { name: 'Larger storage', description: 'Extra 1TB of cloud save', price: '$2/mo', isSelected: false },
-    { name: 'Customizable Profile', description: 'Custom theme on your profile', price: '$2/mo', isSelected: false },
+    { name: 'Online service', description: 'Access to multiplayer games', monthlyPrice: 1, yearlyPrice: 10, isSelected: false },
+    { name: 'Larger storage', description: 'Extra 1TB of cloud save', monthlyPrice: 2, yearlyPrice: 20, isSelected: false },
+    { name: 'Customizable Profile', description: 'Custom theme on your profile', monthlyPrice: 2, yearlyPrice: 20, isSelected: false },
   ];
 
   constructor(private router: Router, private formDataService: FormDataService) {}
+
+  ngOnInit(): void {
+    this.isYearly = this.formDataService.getFormData().selectPlan.isYearly;
+  }
 
   toggleAddOn(index: number): void {
     this.addOns[index].isSelected = !this.addOns[index].isSelected;
@@ -30,5 +35,6 @@ export class AddOnsComponent {
     const selectedAddOns = this.addOns.filter(addOn => addOn.isSelected);
     this.formDataService.setFormData('addOns', selectedAddOns);
     this.router.navigate(['/sign-up/summary']);
+    console.log(this.formDataService.getFormData());
   }
 }
