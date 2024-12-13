5a23b4b6c717409f7c11b306575b551692d43353
Paul Blankson <paul.blankson@amalitech.com>
2024-12-13 10:31:45 +0000

    feat: used isFormDataEmpty method to redirect user to personal info page if fields are not completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterModule } from '@angular/router';
+import { RouterOutlet, RouterModule, Router } from '@angular/router';
+import { FormDataService } from './service/form-data.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'lorem-gaming';
+  constructor(
+    private router: Router,
+    private formDataService: FormDataService
+  ) {}
+
+  ngOnInit(): void {
+    if (!this.formDataService.isFormDataEmpty()) {
+      this.router.navigate(['/sign-up/your-info']);
+    }
+  }
 }
